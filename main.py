5b0a1c58f401a14fdf99eb9f61d876ae68d2c9d8
Sam Johnson <samueljoh@gmail.com>
2023-11-22 17:29:27 -0500

    added llama_chat.py to get responses from llama 70b

--- main.py
+++ main.py
@@ -1,22 +1,15 @@
 import csv
-from models.clinical_bert import query_clinicalbert_with_retry
+from models.llama_chat import query_llama_with_retry
 from utils.data_processing import load_emr_data
 from utils.pubmed_fetch import fetch_pubmed_data, fetch_article_details
 
-def create_clinicalbert_prompt(patient_record):
+def create_llama_prompt(patient_record):
+    # Simplified prompt
     prompt = (
-        f"Age: {patient_record['age']}\n"
-        f"Gender: {patient_record['gender']}\n"
-        f"Symptoms: {patient_record['symptoms']}\n"
-        f"Medical History: {patient_record['medical_history']}\n"
-        f"Test Results: {patient_record['test_results']}\n"
-        f"Diagnosis: {patient_record['diagnosis']}\n"
-        f"Medications: {patient_record['medications']}\n"
-        f"Notes: {patient_record['notes']}\n\n"
-        f"A detailed clinical decision or recommendation for this patient is [MASK]."
+        f"Given a {patient_record['age']}-year-old {patient_record['gender']} with {patient_record['symptoms']} "
+        f"and diagnosed with {patient_record['diagnosis']}, what would be your recommendation?"
     )
     return prompt
-
 
 def main():
     emr_data = load_emr_data("data/mock_emr.csv")
@@ -27,20 +20,26 @@ def main():
         article_ids = fetch_pubmed_data(record['diagnosis'])
         articles = fetch_article_details(article_ids) if article_ids else []
 
-        # Create a prompt for ClinicalBERT
-        prompt = create_clinicalbert_prompt(record)  # Updated to pass only one argument
-        print("Sending prompt to ClinicalBERT:", prompt)  # Add this line for debugging
+        # Create a prompt for LLaMa
+        prompt = create_llama_prompt(record)
+        print("Sending prompt to LLaMa:", prompt)  # For debugging
 
-        # Query ClinicalBERT and store the result
-        clinicalbert_response = query_clinicalbert_with_retry(prompt)
-        results.append({'patient_id': record['patient_id'], 'clinicalbert_response': clinicalbert_response})
+        # Query LLaMa and store the result
+        llama_response = query_llama_with_retry(prompt)
+
+        # Extract and print the generated text
+        generated_text = llama_response[0]['generated_text']
+        print("Generated Text:", generated_text)
+
+        results.append({'patient_id': record['patient_id'], 'llama_response': generated_text})
 
     # Write results to a CSV file
-    with open('data/clinicalbert_responses.csv', 'w', newline='') as file:
-        writer = csv.DictWriter(file, fieldnames=['patient_id', 'clinicalbert_response'])
+    with open('data/llama_responses.csv', 'w', newline='') as file:
+        writer = csv.DictWriter(file, fieldnames=['patient_id', 'llama_response'])
         writer.writeheader()
         writer.writerows(results)
 
 if __name__ == "__main__":
     main()
+
 
--- models/llama_chat.py
+++ models/llama_chat.py
@@ -1,12 +1,46 @@
 import requests
+import time
 
-API_URL = "https://api-inference.huggingface.co/models/meta-llama/Llama-2-7b-chat-hf"
-headers = {"Authorization": "Bearer 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"}
+def query_llama(text):
+    API_URL = "https://api-inference.huggingface.co/models/meta-llama/Llama-2-70b-chat-hf"
+    headers = {"Authorization": "Bearer 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"}
+    payload = {"inputs": text}
 
-def query(payload):
-	response = requests.post(API_URL, headers=headers, json=payload)
-	return response.json()
-	
-output = query({
-	"inputs": "Can you please let us know more details about your ",
-})
+    response = requests.post(API_URL, headers=headers, json=payload)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print("Error response from LLaMa:", response.text)  # Print error response
+        raise Exception(f"Error in LLaMa API call: {response.status_code}")
+
+def query_llama_with_retry(text):
+    if not isinstance(text, str):
+        raise ValueError("Text must be a string")
+
+    for attempt in range(5):
+        try:
+            response = requests.post(
+                'https://api-inference.huggingface.co/models/meta-llama/Llama-2-70b-chat-hf',
+                headers={'Authorization': f'Bearer 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'},
+                json={'inputs': text}
+            )
+            if response.status_code == 200:
+                return response.json()
+            else:
+                print(f"Error response from LLaMa: {response.text}")
+                print(f"Attempt {attempt + 1}/5 failed: Error in LLaMa API call: {response.status_code}")
+        except Exception as e:
+            print(f"Attempt {attempt + 1}/5 failed: {str(e)}")
+            time.sleep(1)
+    raise Exception("Failed to get a valid response from LLaMa after 5 attempts")
+
+
+
+# Example usage
+if __name__ == "__main__":
+    sample_text = "Please provide a detailed clinical decision or recommendation for a patient with the following symptoms: [symptoms]."
+    try:
+        result = query_llama_with_retry(sample_text)
+        print(result)
+    except Exception as e:
+        print(f"Failed to get response from LLaMa: {e}")

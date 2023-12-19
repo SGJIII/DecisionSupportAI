a6a5c7d5f9b73276ccc749783547855461a2b72c
Sam Johnson <samueljoh@gmail.com>
2023-12-18 22:17:21 -0700

    fixed issues with the format of the values returned. But now getting 4101 warning from API response.

--- fhir/fhir_client.py
+++ fhir/fhir_client.py
@@ -16,10 +16,10 @@ class FhirClient:
                 raise Exception("Authentication required")
 
             patient_data = self.get_basic_patient_data(fhir_id)
-            patient_data.update(self.get_medication_data(fhir_id))
-            patient_data.update(self.get_allergy_data(fhir_id))
-            patient_data.update(self.get_condition_data(fhir_id))
-            patient_data.update(self.get_social_history_data(fhir_id))
+            patient_data['medications'] = self.get_medication_data(fhir_id)
+            patient_data['allergies'] = self.get_allergy_data(fhir_id)
+            patient_data['conditions'] = self.get_condition_data(fhir_id)
+            patient_data['social_history'] = self.get_social_history_data(fhir_id)
 
             return patient_data
         except Exception as e:
@@ -71,11 +71,19 @@ class FhirClient:
 
         return patient_data
 
+    def calculate_age(self, birth_date_str):
+        """Calculate age from birth date string (YYYY-MM-DD)."""
+        birth_date = datetime.datetime.strptime(birth_date_str, '%Y-%m-%d').date()
+        today = datetime.date.today()
+        age = today.year - birth_date.year - ((today.month, today.day) < (birth_date.month, birth_date.day))
+        return age
+
     def get_medication_data(self, fhir_id):
         headers = {'Authorization': f'Bearer {self.token}'}
         medication_url = f"{self.base_url}/MedicationRequest?patient={fhir_id}"
         try:
             response = requests.get(medication_url, headers=headers)
+            current_app.logger.debug(f"Medication API Response: {response.text}")
 
             if response.status_code != 200:
                 error_message = f"Failed to retrieve medication data: {response.text}, Status Code: {response.status_code}"
@@ -89,19 +97,21 @@ class FhirClient:
 
             root = ET.fromstring(response.text)
             ns = {'fhir': 'http://hl7.org/fhir'}
-            medications = []
-            for entry in root.findall('.//fhir:entry', ns):
-                medication_display = entry.find('.//fhir:medicationReference/fhir:display', ns)
-                if medication_display is not None and medication_display.attrib.get('value'):
-                    medications.append(medication_display.attrib['value'])
-                else:
-                    current_app.logger.debug(f"Missing or empty 'display' element in MedicationRequest: {ET.tostring(entry)}")
+            medication_display = root.find('.//fhir:medicationReference/fhir:display', ns)
+            if medication_display is not None:
+                medication_display_value = medication_display.attrib.get('value')
+                if not medication_display_value:  # If 'value' attribute is not found, set a default
+                    medication_display_value = 'Unknown medication'
+            else:
+                medication_display_value = 'Unknown medication'
 
-            return {'medications': medications}
+            # Return the medication information as a string
+            return medication_display_value
+
         except requests.exceptions.RequestException as e:
             current_app.logger.error(f"Request error in get_medication_data: {e}")
             raise
-
+    
 
     def get_allergy_data(self, fhir_id):
         headers = {'Authorization': f'Bearer {self.token}'}
@@ -114,32 +124,41 @@ class FhirClient:
                 current_app.logger.error(error_message)
                 raise Exception(error_message)
 
-            # Check if response is empty
             if not response.text:
                 error_message = "Empty response received from allergy data API"
                 current_app.logger.error(error_message)
                 raise Exception(error_message)
 
-            # Log the response for debugging
             current_app.logger.debug(f"Allergy API Response: {response.text}")
 
-            # Parse the XML response
             root = ET.fromstring(response.text)
             ns = {'fhir': 'http://hl7.org/fhir'}
             allergies = []
             for entry in root.findall('.//fhir:entry', ns):
                 allergy_text = entry.find('.//fhir:code/fhir:text', ns)
-                if allergy_text is not None:
+                # Append to allergies list if allergy_text is not None and has text
+                if allergy_text is not None and allergy_text.text:
                     allergies.append(allergy_text.text)
+                else:
+                    # If no specific allergy text is found, use a general description
+                    general_status = entry.find('.//fhir:verificationStatus/fhir:coding/fhir:display', ns)
+                    if general_status is not None and general_status.text:
+                        allergies.append(general_status.text)
 
-            return {'allergies': allergies}
+            # Join the list of allergies into a single string, or provide a default if empty
+            allergies_str = ', '.join(allergies) if allergies else 'No known allergies'
+            
+            return allergies_str
+
         except requests.exceptions.RequestException as e:
             current_app.logger.error(f"Request error in get_allergy_data: {e}")
             raise
+
         
     def get_condition_data(self, fhir_id):
         headers = {'Authorization': f'Bearer {self.token}'}
-        condition_url = f"{self.base_url}Condition?patient={fhir_id}"
+        # Ensure to use the correct 'category' parameter as per the API documentation
+        condition_url = f"{self.base_url}Condition?patient={fhir_id}&category=medical-history"
         try:
             response = requests.get(condition_url, headers=headers)
 
@@ -148,25 +167,29 @@ class FhirClient:
                 current_app.logger.error(error_message)
                 raise Exception(error_message)
 
-            # Check if response is empty
             if not response.text:
                 error_message = "Empty response received from condition data API"
                 current_app.logger.error(error_message)
                 raise Exception(error_message)
 
-            # Log the response for debugging
             current_app.logger.debug(f"Condition API Response: {response.text}")
 
-            # Parse the XML response
             root = ET.fromstring(response.text)
             ns = {'fhir': 'http://hl7.org/fhir'}
-            condition_list = []
+            conditions = []
             for entry in root.findall('.//fhir:entry', ns):
-                condition_text = entry.find('.//fhir:code/fhir:text', ns)
-                if condition_text is not None:
-                    condition_list.append(condition_text.text)
+                condition = entry.find('.//fhir:Condition', ns)
+                if condition is not None:
+                    # Extract the display value of the condition code
+                    condition_code = condition.find('.//fhir:code/fhir:coding/fhir:display', ns)
+                    if condition_code is not None and condition_code.text:
+                        conditions.append(condition_code.text)
 
-            return {'conditions': condition_list}
+            # Join the list of conditions into a single string, or provide a default if empty
+            conditions_str = ', '.join(conditions) if conditions else 'No known conditions'
+            
+            return conditions_str
+
         except requests.exceptions.RequestException as e:
             current_app.logger.error(f"Request error in get_condition_data: {e}")
             raise
@@ -182,44 +205,34 @@ class FhirClient:
                 current_app.logger.error(error_message)
                 raise Exception(error_message)
 
-            # Check if response is empty
             if not response.text:
                 error_message = "Empty response received from social history data API"
                 current_app.logger.error(error_message)
                 raise Exception(error_message)
 
-            # Log the response for debugging
             current_app.logger.debug(f"Social History API Response: {response.text}")
 
-            # Parse the XML response
             root = ET.fromstring(response.text)
             ns = {'fhir': 'http://hl7.org/fhir'}
-            social_history_list = []
+            social_histories = []
             for entry in root.findall('.//fhir:entry', ns):
                 observation = entry.find('.//fhir:Observation', ns)
                 if observation is not None:
+                    # Extracting display text for the social history observation
                     display_text = observation.find('.//fhir:code/fhir:text', ns)
-                    if display_text is not None:
-                        social_history_list.append(display_text.text)
+                    if display_text is not None and display_text.text:
+                        social_histories.append(display_text.text)
 
                     # Extracting from valueCodeableConcept if available
                     value_codeable_concept = observation.find('.//fhir:valueCodeableConcept/fhir:text', ns)
-                    if value_codeable_concept is not None:
-                        social_history_list.append(value_codeable_concept.text)
+                    if value_codeable_concept is not None and value_codeable_concept.text:
+                        social_histories.append(value_codeable_concept.text)
 
-            return {'social_history': social_history_list}
+            # Join the list of social history items into a single string, or provide a default if empty
+            social_history_str = ', '.join(social_histories) if social_histories else 'No social history assessed'
+            
+            return social_history_str
+
         except requests.exceptions.RequestException as e:
             current_app.logger.error(f"Request error in get_social_history_data: {e}")
             raise
-
-    def calculate_age(self, birth_date_str):
-        """Calculate age from birth date string (YYYY-MM-DD)."""
-        birth_date = datetime.datetime.strptime(birth_date_str, '%Y-%m-%d').date()
-        today = datetime.date.today()
-        age = today.year - birth_date.year - ((today.month, today.day) < (birth_date.month, birth_date.day))
-        return age
-
-
-
-
-

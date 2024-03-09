a9bcc5fd8ee71863546dee5856d7fc9f8d852b1d
Sam Johnson <samueljoh@gmail.com>
2024-03-09 03:18:21 -0600

    removed second file

--- DecisionSupportAI/utils/epic_data.py
+++
@@ -1,7 +0,0 @@
-from fhir.fhir_client import FHIRClient
-from flask import current_app
-
-def fetch_patient_data(access_token, patient_id):
-    fhir_client = FHIRClient(current_app.config['EPIC_FHIR_URL'], access_token)
-    return fhir_client.get_patient(patient_id)
-
